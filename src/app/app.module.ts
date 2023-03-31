43dcc88a1d4e103abbc45b1f5407c1ecc8353937
AhmedMoneimm <ahmedmoneimaly@gmail.com>
2023-03-31 09:18:31 +0200

    [Add] Login Text Field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFieldComponent } from './components/login-field/login-field.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFieldComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
