253cfa4448c3974e818189c58bb5e1747b98b550
Yehia Samak <yehia.sherif@ripple-innovations.com>
2023-04-23 00:56:31 +0200

    [fix] the fixed part in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -6,13 +6,17 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sportingScoutFrontend';
   constructor(private route: ActivatedRoute,
     private router: Router )
     {
       this.route=route
-      this.router=router  
+      this.router=router
+    }
+
+    ngOnInit(): void {
+      this.router.navigate(["fakehome"], { relativeTo: this.route });
     }
 
 }
