9c921dd048a5f78181f9faeeea9aa32625298018
AhmedMoneimm <ahmedmoneimaly@gmail.com>
2023-04-26 10:41:22 +0200

    [Add] Login Screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFieldComponent } from './components/login-field/login-field.component';
+import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFieldComponent
+    LoginFieldComponent,
+    LoginScreenComponent
   ],
   imports: [
     BrowserModule,
