e707911269d447055f03b818ac2ff493fafbb4f6
Mohamed Hazem <moh.hazemhf@gmail.com>
2023-05-15 20:42:17 +0300

    fixed routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import 'tslib';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginScreenComponent } from './components/Login/login-screen/login-screen.component';
+import { HomeComponent } from './components/home/screen/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home' ,component: HomeComponent},
+  {path: 'login', component: LoginScreenComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
