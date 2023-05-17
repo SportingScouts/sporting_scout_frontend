79ea674d8136941c579d2e8cff84a90555846b4f
Mohamed Hazem <moh.hazemhf@gmail.com>
2023-05-17 14:50:52 +0300

    missing eye in right place and password regex

--- src/app/components/FirstTimeLogin/first-time-forum/first-time-forum.component.ts
+++ src/app/components/FirstTimeLogin/first-time-forum/first-time-forum.component.ts
@@ -11,18 +11,16 @@ export class FirstTimeForumComponent {
   passwordInvalid: boolean = false;
   formLoading: boolean = false;
   passwordFieldType: string = 'password';
+  readyToRoute: boolean=false;
 
 
-  private validatePassword(password: string) {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*?[0-9])(?=.*?[!#%])[A-Za-z0-9!#%]{8,32}$/;
-    return passwordRegex.test(password);
-  }
   togglePasswordVisibility() {
     this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
   }
   matchcriteria(){
+    console.log(this.password)
     console.log(this.validatePassword(this.password))
-    if (this.password!=this.Repassword )
+    if (this.password!=this.Repassword || this.password=='')
     {
       console.log("dont match");
       this.passwordInvalid=true;
@@ -30,6 +28,12 @@ export class FirstTimeForumComponent {
     else {
       console.log("match");
       this.passwordInvalid=false;
+      this.readyToRoute=true;
     }
+  }
+  private validatePassword(password: string) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!#%])[A-Za-z\d!#%]{8,32}$/;
+    console.log(password)
+    return passwordRegex.test(password);
   }
 }
